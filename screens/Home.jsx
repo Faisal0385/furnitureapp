3e841981bf04a32e50c3b88e106fca273de3958f
Personal <faisaltez@gmail.com>
2024-10-02 12:47:45 +0600

    Using StyleSheet from outside of  parent component

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import styles from "./styles/home.style";
 
 export default function Home() {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Text style={styles.textStyle}>Home</Text>
     </SafeAreaView>
-  )
+  );
 }
