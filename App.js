e7a0e3dc1cbfa8e230c7133eb01bd9f5df9099ac
Personal <faisaltez@gmail.com>
2024-10-02 11:32:25 +0600

    how to add custome fonts

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    extraBold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    mediun: require("./assets/fonts/Poppins-Medium.ttf"),
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    semiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "semiBold",
+    fontSize: 18,
   },
 });
